a3caa389bce1091b9a02bf656b646c9967eea42a
马千里 <301063915@qq.com>
2017-04-26 08:29:38 +0800

    去掉了storyboard

--- Example/MqlClock/AppDelegate.m
+++ Example/MqlClock/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +17,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    ViewController *vc = [[ViewController alloc]init];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
